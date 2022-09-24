28cece32381485057b6d56db9f8d8d05d4049456
Saeqa Sultani <saeqasultani@gmail.com>
2022-09-24 13:28:11 +0430

    Refactor the Calculator component (components/Calculator.js) from a class based component to a functional component with hooks.

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,57 +1,52 @@
-import { Component } from 'react';
 import './calculator.css';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
 // eslint-disable-next-line react/prefer-stateless-function
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-    handleEvent = (e) => {
-      const value = e.target.textContent;
-      const calculat = calculate(this.state, value);
-      this.setState(calculat);
-    };
+  const handleEvent = (e) => {
+    const value = e.target.textContent;
+    const calculat = calculate(state, value);
+    setState(calculat);
+  };
 
-    render() {
-      const { total, operation, next } = this.state;
-      return (
-        <div className="calculat">
-          <div className="input">
-            {total}
-            {operation}
-            {next}
-          </div>
-          <div className="parent">
-            <button className="button" type="submit" onClick={this.handleEvent}>AC</button>
-            <button className="button" type="submit" onClick={this.handleEvent}>+/-</button>
-            <button className="button" type="submit" onClick={this.handleEvent}>%</button>
-            <button className="button-color" type="submit" onClick={this.handleEvent}>+</button>
-            <button className="button" type="submit" onClick={this.handleEvent}>7</button>
-            <button className="button" type="submit" onClick={this.handleEvent}>8</button>
-            <button className="button" type="submit" onClick={this.handleEvent}>9</button>
-            <button className="button-color" type="submit" onClick={this.handleEvent}>x</button>
-            <button className="button" type="submit" onClick={this.handleEvent}>4</button>
-            <button className="button" type="submit" onClick={this.handleEvent}>5</button>
-            <button className="button" type="submit" onClick={this.handleEvent}>6</button>
-            <button className="button-color" type="submit" onClick={this.handleEvent}>-</button>
-            <button className="button" type="submit" onClick={this.handleEvent}>1</button>
-            <button className="button" type="submit" onClick={this.handleEvent}>2</button>
-            <button className="button" type="submit" onClick={this.handleEvent}>3</button>
-            <button className="button-color" type="submit" onClick={this.handleEvent}>+</button>
-            <button className="button-zero" type="submit" onClick={this.handleEvent}>0</button>
-            <button className="button" type="submit" onClick={this.handleEvent}>.</button>
-            <button className="button-color" type="submit" onClick={this.handleEvent}>=</button>
-          </div>
-        </div>
-      );
-    }
-}
+  const { total, operation, next } = state;
+  return (
+    <div className="calculat">
+      <div className="input">
+        {total}
+        {operation}
+        {next}
+      </div>
+      <div className="parent">
+        <button className="button" type="submit" onClick={handleEvent}>AC</button>
+        <button className="button" type="submit" onClick={handleEvent}>+/-</button>
+        <button className="button" type="submit" onClick={handleEvent}>%</button>
+        <button className="button-color" type="submit" onClick={handleEvent}>+</button>
+        <button className="button" type="submit" onClick={handleEvent}>7</button>
+        <button className="button" type="submit" onClick={handleEvent}>8</button>
+        <button className="button" type="submit" onClick={handleEvent}>9</button>
+        <button className="button-color" type="submit" onClick={handleEvent}>x</button>
+        <button className="button" type="submit" onClick={handleEvent}>4</button>
+        <button className="button" type="submit" onClick={handleEvent}>5</button>
+        <button className="button" type="submit" onClick={handleEvent}>6</button>
+        <button className="button-color" type="submit" onClick={handleEvent}>-</button>
+        <button className="button" type="submit" onClick={handleEvent}>1</button>
+        <button className="button" type="submit" onClick={handleEvent}>2</button>
+        <button className="button" type="submit" onClick={handleEvent}>3</button>
+        <button className="button-color" type="submit" onClick={handleEvent}>+</button>
+        <button className="button-zero" type="submit" onClick={handleEvent}>0</button>
+        <button className="button" type="submit" onClick={handleEvent}>.</button>
+        <button className="button-color" type="submit" onClick={handleEvent}>=</button>
+      </div>
+    </div>
+  );
+};
 
 export default Calculator;
